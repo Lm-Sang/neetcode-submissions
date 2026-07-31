d25e1d277b06a32b7834d43dbe346515ced52db5
Lê Minh Sang <minhsangle.contact@gmail.com>
2026-07-31 23:19:40 +0700

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        // 40-41, 91-93, 123-125 
+        Stack<Character> stack = new Stack<>();
+        char c,a;
+        stack.add(s.charAt(0));
+        for (int i = 1; i < s.length(); i++){
+            a = s.charAt(i);
+            if (a == ')'){
+                if (stack.isEmpty())return false;
+                c = stack.pop();
+                if ((int) c + 1 != (int) a) return false;
+                continue;
+            }
+            if (a == '}'){
+                if (stack.isEmpty())return false;
+                c = stack.pop();
+                if ((int) c + 2 != (int) a) return false;
+                continue;
+            }
+            if (a == ']'){
+                if (stack.isEmpty())return false;
+                c = stack.pop();
+                if ((int) c + 2 != (int) a) return false;
+                continue;
+            }
+            stack.add(a);
+        }
+        return true;
+    }
+}
