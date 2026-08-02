1756b6a590df331c4d28df7ce3ae94eaa253123c
Lê Minh Sang <minhsangle.contact@gmail.com>
2026-08-02 21:00:06 +0700

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            int temp = target - numbers[i];
+
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[j] == temp) {
+                    return new int[]{i + 1, j + 1};
+                }
+            }
+        }
+
+        return new int[]{0, 0};
+    }
+}
