469153d6c733dca69aac177610a1d5906655ca17
Lê Minh Sang <minhsangle.contact@gmail.com>
2026-09-08 23:41:24 +0700

    Add: kth-largest-element-in-an-array - submission-0

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int target = nums.length - k;
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int pivotIndex = partition(nums, left, right);
+
+            if (pivotIndex == target) {
+                return nums[pivotIndex];
+            } else if (pivotIndex < target) {
+                left = pivotIndex + 1;
+            } else {
+                right = pivotIndex - 1;
+            }
+        }
+        return -1;
+    }
+
+    private int partition(int[] nums, int left, int right) {
+        int pivot = nums[right];
+        int p = left;
+        for (int i = left; i < right; i++) {
+            if (nums[i] <= pivot) {
+                swap(nums, i, p);
+                p++;
+            }
+        }
+        swap(nums, p, right);
+        return p;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
